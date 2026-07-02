46aa56aacea4083440b4650b19f46a4940505a79
amantya <amantyaindrayeni97@gmail.com>
2026-07-02 17:19:05 +0800

    proses login

---
+++ auth/proses_login.php
@@ -0,0 +1,34 @@
+<?php
+
+session_start();
+
+require '../config/Database.php';
+require '../classes/User.php';
+
+$db = new Database();
+$conn = $db->connect();
+
+$user = new User($conn);
+
+$data = $user->login(
+    $_POST['email']
+);
+
+if (
+    $data &&
+    password_verify(
+        $_POST['password'],
+        $data['password']
+    )
+) {
+
+    $_SESSION['user'] = $data['id'];
+    $_SESSION['nama'] = $data['nama'];
+    $_SESSION['role'] = $data['nama_role'];
+
+    header("Location: ../dashboard/index.php");
+
+} else {
+
+    echo "Email atau password salah";
+}
